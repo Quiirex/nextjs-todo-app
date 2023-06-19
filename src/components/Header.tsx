43e852063cf75735fe8a35806917cb5e39df0691
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-06-19 17:54:39 +0200

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Link from 'next/link';
+
+type HeaderProps = {
+  title: string;
+};
+
+export default function Header({ title }: HeaderProps) {
+  return (
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">{title}</h1>
+      <Link
+        className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        href="/new"
+      >
+        New
+      </Link>
+    </header>
+  );
+}
